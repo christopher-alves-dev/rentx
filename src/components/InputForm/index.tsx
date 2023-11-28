c81198d1240c0c5d2bed5c99d13a4d77240437a2
Christopher Alves <christopher.alves@portalsolar.com.br>
2023-11-27 23:06:58 -0300

    feat: improvements on inputForm component

--- src/components/InputForm/index.tsx
+++ src/components/InputForm/index.tsx
@@ -1,6 +1,6 @@
 import { Feather } from '@expo/vector-icons';
 import React, { useState } from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
+import { useController } from 'react-hook-form';
 import { TextInputProps } from 'react-native';
 import { useTheme } from 'styled-components/native';
 
@@ -10,50 +10,45 @@ import { ProfileFormFields } from '../../screens/Profile/hooks/useForm';
 interface Props extends TextInputProps {
   name: keyof ProfileFormFields;
   iconName: React.ComponentProps<typeof Feather>['name']; // queremos tipar apenas os nomes dos icones
+  control: any;
 }
 
-export const InputForm = ({ name, iconName, ...rest }: Props) => {
+export const InputForm = ({ name, iconName, control, ...rest }: Props) => {
   const theme = useTheme();
-  const { control } = useFormContext();
+  const { field } = useController({
+    control,
+    name,
+    defaultValue: '',
+  });
 
   const [isFocused, setIsFocused] = useState(false);
 
   return (
     <S.Container>
-      <Controller
-        name={name}
-        control={control}
-        render={({ field: { onBlur, onChange, value } }) => {
-          return (
-            <>
-              <S.IconContainer isFocused={isFocused}>
-                <Feather
-                  name={iconName}
-                  size={24}
-                  color={
-                    isFocused || !!value
-                      ? theme.colors.main
-                      : theme.colors.textDetail
-                  }
-                />
-              </S.IconContainer>
+      <S.IconContainer isFocused={isFocused}>
+        <Feather
+          name={iconName}
+          size={24}
+          color={
+            isFocused || !!field.value
+              ? theme.colors.main
+              : theme.colors.textDetail
+          }
+        />
+      </S.IconContainer>
 
-              <S.InputText
-                onFocus={() => {
-                  setIsFocused(true);
-                }}
-                onBlur={() => {
-                  setIsFocused(false);
-                  onBlur();
-                }}
-                isFocused={isFocused}
-                onChangeText={onChange}
-                value={String(value)}
-                {...rest}
-              />
-            </>
-          );
+      <S.InputText
+        value={field.value}
+        onChangeText={field.onChange}
+        onFocus={() => {
+          setIsFocused(true);
         }}
+        onBlur={() => {
+          setIsFocused(false);
+          field.onBlur();
+        }}
+        isFocused={isFocused}
+        {...rest}
       />
     </S.Container>
   );
