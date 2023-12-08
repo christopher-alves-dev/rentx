1dd9236aeedaaa1bcdbf565422f3fa88738bf0ff
Christopher Alves <christopher.alves@portalsolar.com.br>
2023-12-07 23:03:26 -0300

    feat: improved bottom sheet password input to be controlled from form

--- src/components/BottomSheetPasswordInput/index.tsx
+++ src/components/BottomSheetPasswordInput/index.tsx
@@ -1,24 +1,35 @@
 import { Feather } from '@expo/vector-icons';
 import { BottomSheetTextInputProps } from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetTextInput';
 import React, { useState } from 'react';
+import { Control, useController } from 'react-hook-form';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { useTheme } from 'styled-components/native';
 
 import { Container, IconContainer, InputText } from './styles';
 
 interface Props extends BottomSheetTextInputProps {
+  name: string;
   iconName: React.ComponentProps<typeof Feather>['name']; // queremos tipar apenas os nomes dos icones
   value?: string;
+  control: any;
 }
 
 export const BottomSheetPasswordInput = ({
+  name,
   iconName,
   value,
+  control,
   ...rest
 }: Props) => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(true);
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
+
+  const { field } = useController({
+    name,
+    control,
+    defaultValue: '',
+  });
 
   const theme = useTheme();
 
@@ -48,10 +59,17 @@ export const BottomSheetPasswordInput = ({
       </IconContainer>
 
       <InputText
-        secureTextEntry={isPasswordVisible}
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
+        value={field.value}
+        onChangeText={field.onChange}
+        onFocus={() => {
+          setIsFocused(true);
+        }}
+        onBlur={() => {
+          setIsFocused(false);
+          field.onBlur();
+        }}
         isFocused={isFocused}
+        secureTextEntry={isPasswordVisible}
         {...rest}
       />
 
